6817da10ff26b8000b4428ba10094ef46ac5b1af
janeosaur <janewie@gmail.com>
2017-05-03 19:42:07 -0700

    completed step4

--- src/Search.js
+++ src/Search.js
@@ -14,12 +14,21 @@ class Search extends Component {
     console.log('search submitted')
   }
 
+  onInputChange(e) {
+    console.log('input changed')
+    this.setState({
+      query: e.target.value
+    })
+  }
+
   render() {
     return (
       <div className="Home">
 
-        <form onSubmit={ (e) => this.onSearchSubmit(e)}>
-          <input type="text" placeholder="Search Movie Title" value=""></input>
+        <form onSubmit={ (e) => this.onSearchSubmit(e) }>
+          <input
+            onChange={ (e) => this.onInputChange(e) }
+            type="text" placeholder="Search Movie Title" value={this.state.query}></input>
           <button type="submit"> Go! </button>
         </form>
 
