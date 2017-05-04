c5087b4a71a08477df85c97d50f31043d92370a0
janeosaur <janewie@gmail.com>
2017-05-03 17:52:58 -0700

    search button console logs

--- src/Search.js
+++ src/Search.js
@@ -2,11 +2,23 @@
 import React, { Component } from 'react'
 
 class Search extends Component {
+  constructor(){
+    super()
+    this.state = {
+      query: ''
+    }
+  }
+
+  onSearchSubmit(e) {
+    e.preventDefault();
+    console.log('search submitted')
+  }
+
   render() {
     return (
       <div className="Home">
 
-        <form>
+        <form onSubmit={ (e) => this.onSearchSubmit(e)}>
           <input type="text" placeholder="Search Movie Title" value=""></input>
           <button type="submit"> Go! </button>
         </form>
